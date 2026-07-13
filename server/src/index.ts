706959bd60b1b5fb0d880d47adeaaec6c494b828
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2026-07-14 00:20:49 +0530

    feat(middleware): Implement a custom global middleware to handle error message centrally

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,17 +3,19 @@ import cors from "cors"
 import cookieparser from "cookie-parser"
 import { FE_URL, PORT } from "./utils/constants.js";
 import mainRouter from "./route.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 const app = express();
 
 app.use(cors({
     origin: FE_URL,
     credentials: true
-}))
+}));
 
-app.use(express.json())
-app.use(cookieparser())
-app.use("/api/v1", mainRouter)
+app.use(express.json());
+app.use(cookieparser());
+app.use("/api/v1", mainRouter);
+app.use(errorMiddleware)
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
