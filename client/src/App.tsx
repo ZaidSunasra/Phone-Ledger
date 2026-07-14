b9dba1b7bc3c3f06eae3fcf1b0ec960e2679a969
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2026-07-14 21:55:46 +0530

    feat(ui): install toaster component and add to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import { Toaster } from "sonner"
+import Router from "./routes/route"
+
 function App() {
 
   return (
     <>
+      <Router />
+      <Toaster richColors expand={true} closeButton position="bottom-right"/>
     </>
   )
 }
