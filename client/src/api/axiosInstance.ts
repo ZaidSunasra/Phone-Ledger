cff05144e010d2ca2a22151852d1cf822b7c4e71
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2026-07-25 20:10:35 +0530

    refactor(api): add custom header to axiosinstance to contain data for selected organization

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,7 +1,18 @@
+import { useOrganization } from "@/store/organization.store";
 import axios from "axios";
 
 const URL= import.meta.env.VITE_BE_URL;
 
 const axiosInstance = axios.create({ baseURL: URL, withCredentials: true });
+
+axiosInstance.interceptors.request.use((config) => {
+    const organizationId = useOrganization.getState().selectedOrganization;
+
+    if(organizationId){
+        config.headers['x-organization-id'] = organizationId; 
+    }
+
+    return config;
+})
 
 export default axiosInstance;
