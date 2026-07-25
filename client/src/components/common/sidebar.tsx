b726b19f6a3f38197f53e38333c5cd380e7868b5
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2026-07-25 14:12:27 +0530

    feat(ui): add sidebar header and footer

--- client/src/components/common/sidebar.tsx
+++ client/src/components/common/sidebar.tsx
@@ -1,44 +1,23 @@
-import {
-    Sidebar,
-    SidebarContent,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-    SidebarRail,
-} from "@/components/ui/sidebar"
-import { Boxes, ChartLine, IndianRupee, LayoutDashboard, Settings } from "lucide-react"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarRail, } from "@/components/ui/sidebar"
+import { useAuth } from "@/store/auth.store";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+import { Avatar, AvatarFallback, } from "../ui/avatar";
+import { getInitials } from "@/utils/get-initials";
+import { useOrganization } from "@/store/organization.store";
+import { navData } from "@/config/navigation";
 
-const navData = [
-    {
-        icon: LayoutDashboard,
-        title: "Dashboard",
-        url: "/dashboard"
-    },
-    {
-        icon: IndianRupee,
-        title: "Sales",
-        url: "/dashboard"
-    },
-    {
-        icon: Boxes,
-        title: "Inventory",
-        url: "/dashboard"
-    },
-    {
-        icon: ChartLine,
-        title: "Analytics",
-        url: "/dashboard"
-    },
-    {
-        icon: Settings,
-        title: "Settings",
-        url: "/dashboard"
-    },
-
-]
 
 const AppSidebar = () => {
+
+    const user = useAuth((state) => state.user);
+    const { selectedOrganization, setSelectedOrganization } = useOrganization()
+
+    const organizationOptions =
+        user?.organizations.map(org => ({
+            label: org.name,
+            value: org.id
+        })
+        )
 
     return (
         <Sidebar collapsible="icon">
@@ -46,18 +25,55 @@ const AppSidebar = () => {
                 <h1 className="group-data-[collapsible=icon]:hidden">Phone Ledger</h1>
             </SidebarHeader>
             <SidebarContent>
-                {navData.map((item) => (
-                    <SidebarMenu key={item.title}>
+                <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+                    <Select
+                        items={organizationOptions}
+                        value={selectedOrganization ?? ""}
+                        onValueChange={(value) => setSelectedOrganization(value!)}
+                    >
+                        <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Select shop" />
+                        </SelectTrigger>
+                        <SelectContent alignItemWithTrigger={true}>
+                            <SelectGroup>
+                                <SelectLabel>Your Shops</SelectLabel>
+                                {user?.organizations.map((org) => (
+                                    <SelectItem key={org.id} value={org.id}>
+                                        {org.name}
+                                    </SelectItem>
+                                ))}
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </SidebarGroup>
+                <SidebarGroup>
+                    {navData.map((item) => (
+                        <SidebarMenu key={item.title}>
                             <SidebarMenuItem key={item.title}>
                                 <SidebarMenuButton>
                                     {<item.icon />}
                                     <a href={item.url}>{item.title}</a>
                                 </SidebarMenuButton>
                             </SidebarMenuItem>
-                    </SidebarMenu>
-
-                ))}
+                        </SidebarMenu>
+                    ))}
+                </SidebarGroup>
             </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton>
+                            <Avatar className="h-8 w-8 rounded-lg">
+                                <AvatarFallback className="rounded-lg">{user?.name && getInitials(user?.name)}</AvatarFallback>
+                            </Avatar>
+                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                <span className="truncate font-medium">{user?.name}</span>
+                                <span className="truncate text-xs">{user?.email}</span>
+                            </div>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
             <SidebarRail />
         </Sidebar>
     )
